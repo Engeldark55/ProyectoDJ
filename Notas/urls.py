d51c011e06506628cb8d2cd06c0ea712ae9fe0d9
Engel Dark <morenomoralesluisangel@gmail.com>
2024-04-09 18:27:33 -0600

    avance

--- Notas/urls.py
+++ Notas/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
 from . import views
+
+app_name = 'Notas'
+
 urlpatterns = [
-    path('', views.IndexNotas, name='notas_index')
+    path('', views.IndexNotas, name='notas_index'),
+    path('actualizar/<int:pk>', views.ConfimacionTarea, name='confimar_tarea'),
+    path('nuevo/', views.AñadirTarea, name='nuevo')
 ]
--- Notas/views.py
+++ Notas/views.py
@@ -1,9 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Tarea
+from datetime import datetime
+from .forms import TareaForm
 
 # Create your views here.
 def IndexNotas(req):
+    ListadoTareas = Tarea.objects.filter(estado = 'not ok')
     context = {
         'titulo': 'Recordatorios.',
-        'estado': 'active'
+        'estado': 'active',
+        'formulario': '',
+        'lista': ListadoTareas,   
+        'fecha': datetime.now
     }
     return render(req, 'Notas/index.html', context)
+
+def ConfimacionTarea(request, pk):
+    ListadoTarea = Tarea.objects.get(pk=pk)
+    if request.method == 'POST':
+        ListadoTarea.delete()
+    return redirect('Notas:notas_index')
+
+def AñadirTarea(req):
+    NuevoForm = TareaForm
+    context = {
+        'titulo': 'Recordatorios.',
+        'form':NuevoForm
+    }
+    return render(req, 'Notas/components/añadir.html', context)
